d568d3ff6f2cac262fe8fa1072476eece55622ea
Igor Lucas <igorescritor@hotmail.com>
2021-06-13 19:06:39 -0300

    Aula dois: usando o Mockito

--- Digital_Innovation_One/src/test/java/com/teste/CalculadoraTest.java
+++ Digital_Innovation_One/src/test/java/com/teste/CalculadoraTest.java
@@ -1,6 +1,8 @@
 package com.teste;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -11,6 +13,17 @@ public class CalculadoraTest {
 		Calculadora calc = new Calculadora();
 		int soma = calc.soma("1+1+3");
 		assertEquals(5, soma);
+	}
+	
+	@Test
+	public void somarComMock() {
+		Calculadora calc = mock(Calculadora.class);
+		
+		when(calc.soma("1+2")).thenReturn(10);
+		
+		int resultado = calc.soma("1+2");
+		
+		assertEquals(10, resultado);
 	}
 
 }
